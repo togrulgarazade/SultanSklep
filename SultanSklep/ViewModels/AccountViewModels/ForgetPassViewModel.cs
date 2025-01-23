e0304a303afb6a23dc7add0e3dc5b268187da81d
Togrul Garazade <togrulgarazade@gmail.com>
2025-01-23 21:55:34 +0400

    update forget and reset pass operations

--- SultanSklep/ViewModels/AccountViewModels/ForgetPassViewModel.cs
+++ SultanSklep/ViewModels/AccountViewModels/ForgetPassViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,8 @@ namespace SultanSklep.ViewModels.AccountViewModels
     {
         public string Email { get; set; }
 
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
     }
 }
