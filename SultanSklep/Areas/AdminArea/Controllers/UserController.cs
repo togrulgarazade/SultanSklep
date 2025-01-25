21f7bffd3c683b5998e1b43f6da20697764fb71a
Ismayil <mohsumovismayil@gmail.com>
2025-01-26 00:36:40 +0100

    Adding users to admin manel

---
+++ SultanSklep/Areas/AdminArea/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SultanSklep.DataAccessLayer;
+using SultanSklep.Models;
+using SultanSklep.ViewModels.AccountViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SultanSklep.Areas.AdminArea.Controllers
+{
+
+    [Area("AdminArea")]
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private AppDbContext _context { get; }
+
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _context.Users.ToListAsync();
+            ApplicationUser appUser = await _userManager.GetUserAsync(HttpContext.User);
+
+            var userViewModels = new UserViewModel()
+            {
+                ApplicationUsers = users
+            };
+
+
+            return View(userViewModels);
+        }
+    }
+}
