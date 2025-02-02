cf0bb541809268b5ab0205763f5be446b24f0640
Ismayil <mohsumovismayil@gmail.com>
2025-02-02 17:44:42 +0100

    Admin panel

---
+++ SultanSklep/Areas/AdminArea/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SultanSklep.Areas.AdminArea.Controllers
+{ 
+    [Area("AdminArea")]
+    [Authorize(Roles = "Admin")]
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> NewOrders()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> PendingOrders()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SendedOrders()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CompletedOrders()
+        {
+            return View();
+        }
+    }
+
+}
